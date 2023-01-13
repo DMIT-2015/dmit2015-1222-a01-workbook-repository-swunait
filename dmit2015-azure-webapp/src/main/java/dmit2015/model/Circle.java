901b1491c17f60d96725954dc1c1d266f109dfa5
Sam Wu <swu@nait.ca>
2023-01-13 11:50:48 -0700

    created Circle class

--- dmit2015-azure-webapp/src/main/java/dmit2015/model/Circle.java
+++ dmit2015-azure-webapp/src/main/java/dmit2015/model/Circle.java
@@ -43,4 +43,37 @@ public class Circle {
     {
         return 2 * Math.PI * radius;
     }
+
+    public static void main(String[] args)
+    {
+        // Create a Circle with a radius of 1
+        Circle circle1 = new Circle();
+        // The radius of circle1 should be 1,
+        // area should be 3.14159
+        // perimeter should be 6.28
+        System.out.printf("The radius of circle1 is %s\n", circle1.getRadius());
+        System.out.printf("The area of circle1 is %.5f\n", circle1.Area());
+        System.out.printf("The perimeter of circle1 is %.2f\n", circle1.Perimeter());
+
+        // Change the radius of circle1 to 5
+        circle1.setRadius(5);
+        // The radius of circle1 should be 5,
+        // area should be 78.53982
+        // perimeter should be 31.41593
+        System.out.printf("The radius of circle1 is %s\n", circle1.getRadius());
+        System.out.printf("The area of circle1 is %.5f\n", circle1.Area());
+        System.out.printf("The perimeter of circle1 is %.2f\n", circle1.Perimeter());
+
+        // Change the radius of cirlce1 to -25
+        // It should throw an exception
+        try
+        {
+            circle1.setRadius(-25);
+            System.out.println("A exception should have been thrown");
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
