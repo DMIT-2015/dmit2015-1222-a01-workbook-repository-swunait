f2e2843c5f6e80f4f0cec795e2eaf126c5b9c4a7
Sam Wu <swu@nait.ca>
2023-02-10 21:50:43 -0700

    Fixed logical error in line 41 of CanadianPersonalIncomeTaxRate

--- dmit2015-azure-webapp/src/main/java/dmit2015/model/CanadianPersonalIncomeTaxRate.java
+++ dmit2015-azure-webapp/src/main/java/dmit2015/model/CanadianPersonalIncomeTaxRate.java
@@ -38,7 +38,7 @@ public class CanadianPersonalIncomeTaxRate {
             7) Base Tax Amount
             8) Rounded Base Tax Amount
          */
-        String[] tokens = line.split(line);
+        String[] tokens = line.split(DELIMITER);
         if (tokens.length != 9) {
             throw new RuntimeException("Line does not contain exactly 9 values");
         }
@@ -47,8 +47,8 @@ public class CanadianPersonalIncomeTaxRate {
         int taxYear = Integer.parseInt(tokens[2]);
         int bracketNo = Integer.parseInt(tokens[3]);
         double startingIncome = Double.parseDouble(tokens[4]);
-        Double endingIncome = tokens[5].isBlank() ? null : Double.valueOf(tokens[5]);
-//        Double endingIncome = tokens[5].isBlank() ? Integer.MAX_VALUE : Double.valueOf(tokens[5]);
+//        Double endingIncome = tokens[5].isBlank() ? null : Double.valueOf(tokens[5]);
+        Double endingIncome = tokens[5].isBlank() ? Integer.MAX_VALUE : Double.valueOf(tokens[5]);
         double taxRate = Double.parseDouble(tokens[6]);
         double baseTax = Double.parseDouble(tokens[7]);
         CanadianPersonalIncomeTaxRate currentIncomeTaxRate = new CanadianPersonalIncomeTaxRate();
